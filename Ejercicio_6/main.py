cb04605a5018e377f58db19e668447c51fd35e49
Alexander Nuñez <82907784+Alerxp@users.noreply.github.com>
2022-04-29 04:30:53 -0300

    Add files via upload

---
+++ Ejercicio_6/main.py
@@ -0,0 +1,12 @@
+from claseListaViajeros import ListaViajeros
+
+if __name__ == "__main__":
+    viajeros = ListaViajeros()
+    viajeros.testListaViajeros()
+
+    v = viajeros.maxMillas()
+    print("\n=== Viajero con mayor cantidad de millas ===\n{}".format(v))
+    v += 100
+    print("\n=== Acumula 100 millas ===\n{}".format(v))
+    v -= 100
+    print("\n=== Canjea 100 millas ===\n{}".format(v))
