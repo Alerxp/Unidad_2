c0241eaa9995ac53085910c12f2c64abd28a3191
Alexander Nuñez <82907784+Alerxp@users.noreply.github.com>
2022-08-29 16:04:12 -0300

    Update main.py

--- Ejercicio 1 - Pila secuencial/main.py
+++ Ejercicio 1 - Pila secuencial/main.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     print(p.empty())  # Pila vacía
     p.push(3)
     p.push(1)
-    p.push(5)         # Pila llena
-    p.push(7)         # No se puede apilar
+    p.push(5)
+    p.push(7)         # No se puede apilar, pila llena
     p.unstack()       # Desapilar
     print(p.empty())  # Pila vacía
