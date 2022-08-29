50d8e829122f315c04d1b01e02f8a57c5f8f0227
Alexander Nuñez <82907784+Alerxp@users.noreply.github.com>
2022-08-29 18:42:29 -0300

    Add files via upload

---
+++ Ejercicio 6 - Cola encadenada/main.py
@@ -0,0 +1,15 @@
+from Cola import Cola
+
+if __name__ == '__main__':
+    c = Cola()
+    c.insertar(1)        # Inserta el elemento 1 en la cola
+    c.insertar(2)        # Inserta el elemento 2 en la cola
+    c.insertar(3)        # Inserta el elemento 3 en la cola
+    print(c.suprimir())  # Suprime el elemento 1 (primero en la cola), nuevo primer elemento 2
+    c.insertar(4)        # Inserta el elemento 4 en la cola
+    print(c.suprimir())  # Suprime el elemento 2 (primero en la cola), nuevo primer elemento 3
+    c.insertar(0)        # Inserta el elemento 0 en la cola
+    print(c.suprimir())  # Suprime el elemento 3 (primero en la cola), nuevo primer elemento 4
+    print(c.suprimir())  # Suprime el elemento 4 (primero en la cola), nuevo primer elemento 0
+    print(c.suprimir())  # Suprime el elemento 0, la cola queda vacía
+    print(c.vacia())
